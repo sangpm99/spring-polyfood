300e2a48750f69ba37351209c4e075ffd18988f5
sangpm99 <sangpm72@wru.vn>
2024-03-14 10:36:26 +0700

    update SQL

--- src/main/java/com/example/polyfood/Config/InitializerConfig.java
+++ src/main/java/com/example/polyfood/Config/InitializerConfig.java
@@ -10,74 +10,74 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class InitializerConfig implements ApplicationRunner {
     private final ProvincesInitializer provincesInitializer;
-//    private final DecentralizationInitializer decentralizationInitializer;
-//    private final AccountInitializer accountInitializer;
-//    private final UserInitializer userInitializer;
-//    private final ProductTypeInitializer productTypeInitializer;
-//    private final ProductInitializer productInitializer;
-//    private final ProductDetailInitializer productDetailInitializer;
-//    private final ProductImageInitializer productImageInitializer;
-//    private final ProductReviewInitializer productReviewInitializer;
-//    private final CartInitializer cartInitializer;
-//    private final CartItemInitializer cartItemInitializer;
-//    private final PaymentInitializer paymentInitializer;
-//    private final OrderStatusInitializer orderStatusInitializer;
-//    private final OrdersInitializer ordersInitializer;
-//    private final OrderDetailInitializer orderDetailInitializer;
+    private final DecentralizationInitializer decentralizationInitializer;
+    private final AccountInitializer accountInitializer;
+    private final UserInitializer userInitializer;
+    private final ProductTypeInitializer productTypeInitializer;
+    private final ProductInitializer productInitializer;
+    private final ProductDetailInitializer productDetailInitializer;
+    private final ProductImageInitializer productImageInitializer;
+    private final ProductReviewInitializer productReviewInitializer;
+    private final CartInitializer cartInitializer;
+    private final CartItemInitializer cartItemInitializer;
+    private final PaymentInitializer paymentInitializer;
+    private final OrderStatusInitializer orderStatusInitializer;
+    private final OrdersInitializer ordersInitializer;
+    private final OrderDetailInitializer orderDetailInitializer;
 
     @Autowired
     public InitializerConfig(
-            ProvincesInitializer provincesInitializer
-//            DecentralizationInitializer decentralizationInitializer,
-//            AccountInitializer accountInitializer,
-//            UserInitializer userInitializer,
-//            ProductTypeInitializer productTypeInitializer,
-//            ProductInitializer productInitializer,
-//            ProductDetailInitializer productDetailInitializer,
-//            ProductImageInitializer productImageInitializer,
-//            ProductReviewInitializer productReviewInitializer,
-//            CartInitializer cartInitializer,
-//            CartItemInitializer cartItemInitializer,
-//            PaymentInitializer paymentInitializer,
-//            OrderStatusInitializer orderStatusInitializer,
-//            OrdersInitializer ordersInitializer,
-//            OrderDetailInitializer orderDetailInitializer
+            ProvincesInitializer provincesInitializer,
+            DecentralizationInitializer decentralizationInitializer,
+            AccountInitializer accountInitializer,
+            UserInitializer userInitializer,
+            ProductTypeInitializer productTypeInitializer,
+            ProductInitializer productInitializer,
+            ProductDetailInitializer productDetailInitializer,
+            ProductImageInitializer productImageInitializer,
+            ProductReviewInitializer productReviewInitializer,
+            CartInitializer cartInitializer,
+            CartItemInitializer cartItemInitializer,
+            PaymentInitializer paymentInitializer,
+            OrderStatusInitializer orderStatusInitializer,
+            OrdersInitializer ordersInitializer,
+            OrderDetailInitializer orderDetailInitializer
 
     ) {
         this.provincesInitializer = provincesInitializer;
-//        this.decentralizationInitializer = decentralizationInitializer;
-//        this.accountInitializer = accountInitializer;
-//        this.userInitializer = userInitializer;
-//        this.productTypeInitializer = productTypeInitializer;
-//        this.productInitializer = productInitializer;
-//        this.productDetailInitializer = productDetailInitializer;
-//        this.productImageInitializer = productImageInitializer;
-//        this.productReviewInitializer = productReviewInitializer;
-//        this.cartInitializer = cartInitializer;
-//        this.cartItemInitializer = cartItemInitializer;
-//        this.paymentInitializer = paymentInitializer;
-//        this.orderStatusInitializer = orderStatusInitializer;
-//        this.ordersInitializer = ordersInitializer;
-//        this.orderDetailInitializer = orderDetailInitializer;
+        this.decentralizationInitializer = decentralizationInitializer;
+        this.accountInitializer = accountInitializer;
+        this.userInitializer = userInitializer;
+        this.productTypeInitializer = productTypeInitializer;
+        this.productInitializer = productInitializer;
+        this.productDetailInitializer = productDetailInitializer;
+        this.productImageInitializer = productImageInitializer;
+        this.productReviewInitializer = productReviewInitializer;
+        this.cartInitializer = cartInitializer;
+        this.cartItemInitializer = cartItemInitializer;
+        this.paymentInitializer = paymentInitializer;
+        this.orderStatusInitializer = orderStatusInitializer;
+        this.ordersInitializer = ordersInitializer;
+        this.orderDetailInitializer = orderDetailInitializer;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         // Chạy các logic khởi tạo dữ liệu theo thứ tự mong muốn
         provincesInitializer.run(args);
-//        decentralizationInitializer.run(args);
-//        accountInitializer.run(args);
-//        userInitializer.run(args);
-//        productTypeInitializer.run(args);
-//        productInitializer.run(args);
-//        productDetailInitializer.run(args);
-//        productImageInitializer.run(args);
-//        productReviewInitializer.run(args);
-//        cartInitializer.run(args);
-//        cartItemInitializer.run(args);
-//        paymentInitializer.run(args);
-//        orderStatusInitializer.run(args);
-//        ordersInitializer.run(args);
-//        orderDetailInitializer.run(args);
+        decentralizationInitializer.run(args);
+        accountInitializer.run(args);
+        userInitializer.run(args);
+        productTypeInitializer.run(args);
+        productInitializer.run(args);
+        productDetailInitializer.run(args);
+        productImageInitializer.run(args);
+        productReviewInitializer.run(args);
+        cartInitializer.run(args);
+        cartItemInitializer.run(args);
+        paymentInitializer.run(args);
+        orderStatusInitializer.run(args);
+        ordersInitializer.run(args);
+        orderDetailInitializer.run(args);
     }
 }
